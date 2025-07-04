f7b04d1b06595735e8170f66b0f4f7396dfdc6e9
JuanSegura <sebasthsegura05@gmail.com>
2025-07-04 15:07:19 -0500

    add new view class

--- src/main/java/com/sebasth/alifood/views/LoginView.java
+++ src/main/java/com/sebasth/alifood/views/LoginView.java
@@ -1,4 +1,41 @@
 package com.sebasth.alifood.views;
 
+import com.sebasth.alifood.controllers.LoginController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginView {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label messageLabel;
+
+    private LoginController loginController;
+
+    // Initialize the controller
+    @FXML
+    private void setLoginController(LoginController loginController){
+        this.loginController = loginController;
+    }
+
+    @FXML
+    private void handleLoginButtonAction(ActionEvent event){
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (loginController.authenticateUser(username, password)){
+            messageLabel.setText("Inicio de sesión exitoso.");
+        }
+    }
 }
