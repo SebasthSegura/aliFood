f16915b9a7a1c3bf18c3d91edd41f93514818690
JuanSegura <sebasthsegura05@gmail.com>
2025-07-04 10:53:17 -0500

    code to PasswordUtils.java to encrypted password

--- src/main/java/com/sebasth/alifood/utils/PasswordUtils.java
+++ src/main/java/com/sebasth/alifood/utils/PasswordUtils.java
@@ -1,4 +1,15 @@
 package com.sebasth.alifood.utils;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class PasswordUtils {
+    // Method to hash a password
+    public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt(12)); // Replace with actual hashing logic
+    }
+
+    // Method to verify a password against a hashed password
+    public static boolean verifyPassword(String plainPassword, String hashedPassword) {
+        return BCrypt.checkpw(plainPassword, hashedPassword);
+    }
 }
