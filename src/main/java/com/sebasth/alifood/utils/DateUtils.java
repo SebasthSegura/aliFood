0099f1400df8f8fb6b345413a8fa92438bb4a6a7
JuanSegura <sebasthsegura05@gmail.com>
2025-07-04 10:54:23 -0500

    code to verify days on between dates

--- src/main/java/com/sebasth/alifood/utils/DateUtils.java
+++ src/main/java/com/sebasth/alifood/utils/DateUtils.java
@@ -1,4 +1,17 @@
 package com.sebasth.alifood.utils;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class DateUtils {
+
+    // Method to calculate the difference in days between two dates
+    public static long getDaysBetween(LocalDate startDate, LocalDate endDate) {
+        return ChronoUnit.DAYS.between(startDate, endDate);
+    }
+
+    // Method to check if one date has passed another date by a certain number of days
+    public static boolean isToday(LocalDate date, int daysOffset) {
+        return getDaysBetween(date, LocalDate.now()) > daysOffset;
+    }
 }
