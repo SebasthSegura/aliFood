55aa446aa7060cc6c00529c6287b11278cd7ff0a
JuanSegura <sebasthsegura05@gmail.com>
2025-07-07 14:56:23 -0500

    code update ItemShopList.java

--- src/main/java/com/sebasth/alifood/models/ItemShopList.java
+++ src/main/java/com/sebasth/alifood/models/ItemShopList.java
@@ -4,17 +4,19 @@ public class ItemShopList {
     private int id_Item;
     private String nameItem;
     private String quantity;
+    private String status;
 
     // Create a complete constructor
-    public ItemShopList(int id_Item, String nameItem, String quantity) {
+    public ItemShopList(int id_Item, String nameItem, String quantity, String status) {
         this.id_Item = id_Item;
         this.nameItem = nameItem;
         this.quantity = quantity;
+        this.status = status;
     }
 
     // Create new constructor without id_Item
     public ItemShopList(String nameItem, String quantity) {
-        this(-1, nameItem, quantity);
+        this(-1, nameItem, quantity, "pending");
     }
 
     // Getters and Setters
@@ -41,6 +43,14 @@ public class ItemShopList {
 
     public void setQuantity(String quantity) {
         this.quantity = quantity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     // Override toString method for better representation
