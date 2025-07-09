232def8f4eebe88e85382ea18d7b6815b598c288
JuanSegura <sebasthsegura05@gmail.com>
2025-07-09 10:07:46 -0500

    create code to RegisterController.java manipule data base with UserDAO and add a object User.java

--- src/main/java/com/sebasth/alifood/controllers/RegisterController.java
+++ src/main/java/com/sebasth/alifood/controllers/RegisterController.java
@@ -1,4 +1,29 @@
 package com.sebasth.alifood.controllers;
 
+import com.sebasth.alifood.database.UserDAO;
+import com.sebasth.alifood.models.User;
+import com.sebasth.alifood.utils.PasswordUtils;
+
+import java.sql.SQLException;
+
 public class RegisterController {
+    private UserDAO userDAO;
+    private User newUser;
+
+    // Constructor to inject UserDAO dependency
+    public RegisterController(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    // Method to register a new user
+    public boolean registerUser(String username, String password) throws SQLException {
+        String hashedPassword = PasswordUtils.hashPassword(password);
+        // Check if the user already exists
+        if (userDAO.userExists(username)) {
+            return false; // User already exists
+        }
+        newUser = new User(username, hashedPassword);
+        userDAO.registerUser(newUser);
+        return true; // Registration successful
+    }
 }
