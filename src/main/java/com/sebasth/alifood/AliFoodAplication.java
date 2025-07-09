3997dc6dcd6cf912a3076b463d0ada9b816461a6
JuanSegura <sebasthsegura05@gmail.com>
2025-07-09 11:47:32 -0500

    add code to run application

--- src/main/java/com/sebasth/alifood/AliFoodAplication.java
+++ src/main/java/com/sebasth/alifood/AliFoodAplication.java
@@ -1,20 +1,64 @@
 package com.sebasth.alifood;
 
+import com.sebasth.alifood.controllers.LoginController;
+import com.sebasth.alifood.controllers.RegisterController;
+import com.sebasth.alifood.views.LoginView;
+import com.sebasth.alifood.views.RegisterView;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class AliFoodAplication extends Application {
+
+    private Stage primmaryStage;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AliFoodAplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primmaryStage){
+        this.primmaryStage = primmaryStage;
+        this.primmaryStage.setTitle("AliFood Application");
+        showLoginView();
+    }
+
+    // Method to show the login view
+    public void showLoginView(){
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/com/sebasth/alifood/login-view.fxml"));
+            AnchorPane loginL = loader.load();
+
+            Stage loginStage = new Stage();
+            loginStage.setTitle("Login");
+            loginStage.setScene(new Scene(loginL));
+            LoginView loginView = loader.getController();
+            loginView.setLoginController(new LoginController());
+            loginStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to show the login view
+    public void showRegisterView(){
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/com/sebasth/alifood/register-view.fxml"));
+            AnchorPane registerV = loader.load();
+
+            Stage registerStage = new Stage();
+            registerStage.setTitle("Registro");
+            registerStage.setScene(new Scene(registerV));
+            RegisterView registerView = loader.getController();
+
+            registerView.setRegisterController(new RegisterController());
+            registerStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
